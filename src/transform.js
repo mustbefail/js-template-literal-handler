f08710f276adcfd8607ae986fe10fe83f11aa513
Viktor Kushnir <mustbefail@gmail.com>
2021-04-29 16:28:10 +0300

    level 1 rdy, level80 to go

--- src/transform.js
+++ src/transform.js
@@ -1,5 +1,5 @@
 // assign 80 once you accept a challenge to implement Level 80 requirements
-export const level = 1;
+export const level = 80;
 
 /*
   Add parameters as appropriate.
@@ -7,7 +7,24 @@ export const level = 1;
   Should work with arbitrary function expressions passed.
   https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals#tagged_templates
 */
-export function transform() {}
+export function transform(strings, ...expressions) {
+  const result = expressions.reduce((acc, exp, idx) => {
+    let transformedExp;
+    if (typeof exp === 'function') {
+      transformedExp = exp(strings[idx + 1], expressions[idx + 1]);
+      console.log(transformedExp);
+    } else if (typeof exp === 'number') {
+      transformedExp = exp * 2 + 3;
+    } else if (typeof exp === 'string') {
+      transformedExp = exp.toLowerCase();
+    } else if (typeof exp === 'object') {
+      transformedExp = JSON.stringify(exp);
+    } else transformedExp = exp;
+
+    return `${acc}${transformedExp}${strings[idx + 1]}`;
+  }, strings[0]);
+  return result;
+}
 
 /*
   Level 1: expects no parameters. Returns its own name capitalized.
@@ -19,4 +36,9 @@ export function transform() {}
     Usage example:
       testFunction("some Text", 125) should return 'TESTFUNCTION(someText=125)'
  */
-export function testFunction() {}
+
+export function testFunction(string, exp) {
+  const name = testFunction.name.toUpperCase();
+  const newStr = string.replace(/\s/g, '');
+  return `${name}(${newStr}=${exp})`;
+}
